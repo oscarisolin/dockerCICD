6113a690329d406d5e3165658befed198d81760a
oscar weissenbach <oscar.infos@gmail.com>
2019-05-12 01:01:24 +0200

    added apache with mod_wsgi and python for backend

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,5 +3,8 @@ sed -i s+#PermitRootLogin+PermitRootLogin+ /etc/ssh/sshd_config
 sed -i s+#PubkeyAuthentication+PubkeyAuthentication+ /etc/ssh/sshd_config
 service ssh start
 service ssh status
-touch test.txt && tail -f test.txt
-
+mkdir /usr/local/apache2/htdocs/backend
+echo 'LoadModule wsgi_module /usr/lib/apache2/modules/mod_wsgi.so' >> /usr/local/apache2/conf/httpd.conf
+echo 'Include conf/extra/wsgi.conf' >> /usr/local/apache2/conf/httpd.conf
+httpd-foreground > apache.log
+tail -f apache.log
